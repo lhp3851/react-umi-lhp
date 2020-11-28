b22ffd69fffa348e6cb99fe387644e8b1173467d
Jerry <lhp3851@163.com>
2020-11-28 13:41:51 +0800

    [feature][ 项目]：1. 创（搭）建项目；

---
+++ src/components/table/empty.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Tabs } from 'antd';
+
+class EmptyTable extends React.Component {
+  tabs = [];
+
+  render() {
+    <Tabs></Tabs>;
+  }
+}
+
+export default EmptyTable;
